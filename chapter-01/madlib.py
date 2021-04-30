f37c84a515b3778cc0fc76e1a8cdaa6713e69be5
achyutha-chandan <81483799+achyutha-chandan@users.noreply.github.com>
2021-04-30 07:45:34 -0700

    Create madlib.py

---
+++ chapter-01/madlib.py
@@ -0,0 +1,6 @@
+#madlib.py
+adjective=input("please enter an adjective:")
+noun=input("please enter a noun:")
+verb=input ("please enter a verb ending -ed:")
+print("Your madlib:")
+print("the",adjective,noun,verb,"over the lazy brown dog.")
