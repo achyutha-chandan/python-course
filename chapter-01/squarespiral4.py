43ed90ea6e2e2ec0a348f270be27e21db3de657d
achyutha-chandan <81483799+achyutha-chandan@users.noreply.github.com>
2021-05-02 07:58:06 -0700

    Delete squarespiral4.py

--- chapter-01/squarespiral4.py
+++
@@ -1,9 +0,0 @@
-import turtle
-t = turtle.Pen()
-turtle.bgcolor("black")
-t.speed(0)
-colors = ["red", "yellow", "blue", "green"]
-for x in range(100):
-    t.pencolor( colors[ x % 4] )
-    t.circle(x)
-    t.left(95)
