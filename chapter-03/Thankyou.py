ff38e6f2d518b4ce4f9e20d2633efaf4394d48ed
achyutha-chandan <81483799+achyutha-chandan@users.noreply.github.com>
2021-05-02 22:15:24 -0700

    Create Thankyou.py

---
+++ chapter-03/Thankyou.py
@@ -0,0 +1,8 @@
+#thankyou.py
+my_name = "achyutha"
+my_age = 13
+your_name= input("what is your name? ")
+your_age = input("what is your age? ")
+print("my name is" ,my_name,"and i am",my_age, "years old")
+print("your name is",your_name,"and u are",your_age,"years old")
+print(" thank you for accepting the invite ",your_name,"!!")
