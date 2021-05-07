7e832c38e158d726fcab5f937c01309a9f5f5cca
achyutha-chandan <81483799+achyutha-chandan@users.noreply.github.com>
2021-05-07 02:37:04 -0700

    Delete cards.py

--- Chapter-06/cards.py
+++
@@ -1,8 +0,0 @@
-import random
-suits = ["clubs", "diamonds", "hearts", "spades"]
-faces = ["two", "three", "four", "five", "six",
-         "seven", "eight", "nine","ten", "jack",
-         "queen", "king", "ace"]
-my_face = random.choice(faces)
-my_suit = random.choice(suits)
-print("I have the", my_face, "of", my_suit)
